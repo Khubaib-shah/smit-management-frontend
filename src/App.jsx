c7894619e81c1e61631784510e609f89d0a28c90
khubaib shah <bewithbaacha@gmail.com>
2024-12-08 01:06:16 +0500

    Initialize app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Admin from "./pages/Admin";
+import Students from "./pages/Students";
+import Teachers from "./pages/Teachers";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/students" element={<Students />} />
+      <Route path="/teachers" element={<Teachers />} />
+    </Routes>
+  );
+}
+
+export default App;
